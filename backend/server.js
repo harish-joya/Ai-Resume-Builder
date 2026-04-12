83e7eaddae5cbde48a528a86752c4b683ed21a07
harishjoya <harishjoya90@gmail.com>
2026-04-12 22:28:26 +0530

    fix some issue

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "dist")));
 
 // React Router support (VERY IMPORTANT)
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
